096d0310485c34bbe6e747964670aace6a27a8e9
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-05-03 00:09:20 +0700

    Done

---
+++ js/app.js
@@ -0,0 +1,71 @@
+const breakpoints = {
+    xs: 576,
+    sm: 618,
+    md: 768,
+    lg: 1024
+}
+
+fetch('https://picsum.photos/1080')
+    .then(response => response.json())
+    .then(data => {
+        // Do something with the returned JSON data
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors that occur during the request
+        console.error(error);
+    });
+
+async function getImg(index) {
+
+}
+
+
+const generateGrid = async (props) => {
+    const container = document.querySelector(".container");
+    container.style.gridTemplateColumns = `repeat(${props}, 1fr)`;
+    if (!document.querySelector(".pic")) {
+        for (let i = 0; i < 50; i++) {
+            const response = await fetch('https://picsum.photos/1080');
+            const pic = document.createElement("div");
+            pic.className = "pic";
+
+            const img = document.createElement("img");
+            img.src = response.url;
+
+            const number = document.createElement("div");
+            number.className = "number";
+            number.innerText = i + 1;
+
+            pic.appendChild(img);
+            pic.appendChild(number);
+            container.appendChild(pic);
+        }
+    }
+
+}
+
+const deviceWidth = window.innerWidth;
+if (deviceWidth >= breakpoints.lg) {
+    generateGrid(4);
+} else if (deviceWidth <= breakpoints.md) {
+    generateGrid(3);
+} else if (deviceWidth <= breakpoints.sm) {
+    generateGrid(2);
+} else {
+    generateGrid(1);
+}
+
+addEventListener('resize', () => {
+    const deviceWidth = window.innerWidth;
+    if (deviceWidth >= breakpoints.lg) {
+        generateGrid(4);
+    } else if (deviceWidth >= breakpoints.md) {
+        generateGrid(3);
+    } else if (deviceWidth >= breakpoints.sm) {
+        generateGrid(2);
+    } else {
+        generateGrid(1);
+    }
+})
+
